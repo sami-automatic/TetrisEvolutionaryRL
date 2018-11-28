544970efa8231aaf140ee4f84e4c5e6a50fff0b7
Deniz Seyidoglu <denizsydgl@gmail.com>
2018-11-28 12:41:48 +0300

    Fixed crossover function

--- evolution_agent_bed.py
+++ evolution_agent_bed.py
@@ -20,7 +20,7 @@ def create_random_agents():
 def simulate(times):
     for _ in range(times):
         play_all_agents()
-        eliminate_half_of_agents()
+        eliminate_agents()
         cross_over_and_multiply()
 
 
@@ -44,21 +44,20 @@ def play_one_game(agent):
     return reward_of_game
 
 
-def eliminate_half_of_agents():
+def eliminate_agents():
     global agents, rewards, reward_agent_tuple
     zipped = zip(rewards, agents)
     reward_agent_list = list(zipped)
     reward_agent_tuple = reward_agent_list[:]
     sorted(reward_agent_tuple, key=lambda x: x[0])
-    agents = [reward_agent_tuple[i][1] for i in range(NUMBER_OF_AGENTS // 2)]
+    agents = [reward_agent_tuple[i][1] for i in range(NUMBER_OF_AGENTS // 2 + 1)]   # +1 because we need the last agent
 
 
 def cross_over_and_multiply():
-    global agents, reward_agent_tuple
-    for i in range(NUMBER_OF_AGENTS // 2):
-        print(reward_agent_tuple)
-        top = reward_agent_tuple[i][1]
-        bottom = reward_agent_tuple[i+1][1]
+    global agents
+    for i in range(len(agents) - 1):
+        top = agents[i]
+        bottom = agents[i+1]
         lhs = top.get_attributes()
         rhs = bottom.get_attributes()
         shuffled_attributes = [lhs[x] if np.random.randint(0, 9) % 2 == 0 else rhs[x] for x in range(8)]
--- hedonistic_agent.py
+++ hedonistic_agent.py
@@ -7,7 +7,6 @@ import numpy as np
 
 class Agent(object):
     def __init__(self,
-                 debug=False,
                  column_count=20,
                  row_count=5,
                  touches_another_block_reward=6.0,
@@ -19,7 +18,6 @@ class Agent(object):
                  blockade_penalty=-0.3,
                  bumpiness_penalty=-0.2):
 
-        self.debug = debug
         self.column_count = column_count
         self.row_count = row_count
         self.touches_another_block_reward = touches_another_block_reward
@@ -46,21 +44,17 @@ class Agent(object):
     # TODO: Play all possible combinations and get the biggest scored one.
 
     def play(self, state):
-        action_list = []
+        action_list = []    # action list can be 0, 0, 0, 2  (left, left, left, hard_drop)
         env = TetrisEngine(self.row_count, self.column_count, state)
         obs = env.board
         # done = false
         # while done is false:
         #     obs, reward, done, cleared = env.step(2)
-        #     debug and print("obs: \n", obs, "\nreward:",reward, "\ndone:", done, "\n\n")
+        #     print("obs: \n", obs, "\nreward:",reward, "\ndone:", done, "\n\n")
         #     calculate_score(obs, cleared)
         for i in range(5):
             for _ in range(i):
-                self.debug and print("go right")
                 obs, reward, done, cleared = env.step(1)    # right
-                self.debug and print(
-                    "obs: \n", obs, "\nreward:", reward, "\ndone:", done, "\n\n")
-            self.debug and print("go down")
             obs, reward, done, cleared = env.step(2)        # down
             print("obs: \n", obs, "\ndone:", done)
             self.calculate_score(obs, cleared)
@@ -97,20 +91,14 @@ class Agent(object):
         got_floor_point = False
         for (x, y) in neighbours:
             if j == self.column_count - 1 and not got_floor_point:   # touching the floor
-                self.debug and print("touching the floor, 5 point!")
                 point += self.touches_floor_reward
                 got_floor_point = True
             try:                # has neighboring index
                 if obs[x][y]:   # touches another block
-                    self.debug and print(
-                        "touching another block, 3 point! touches: (%d, %d)" % (x, y))
                     point += self.touches_another_block_reward
             except IndexError:                                  # point is on the edge
                 if y == self.column_count and got_floor_point:  # if we added the floor point, don't add extra 2.5!
-                    self.debug and print("floor point already added.")
                     continue
-                self.debug and print(
-                    "is on edge, 2.5 point! cant find: (%d, %d)" % (x, y))
                 point += self.touches_wall_reward
         return point
 
@@ -150,12 +138,9 @@ class Agent(object):
             for (j, cell) in enumerate(row):
                 # starting position of block. ignore!
                 if self.is_starting_cell(i, j):
-                    self.debug and print("starting position, skipping..")
                     continue
                 if cell:
-                    self.debug and print("i:", i, "\tj:", j, "\tvalue:", cell)
                     edge_point = self.calculate_edge(obs, i, j)
-                    self.debug and print("point:", edge_point)
                     edge_score += edge_point
                 else:
                     try:
@@ -165,8 +150,6 @@ class Agent(object):
                             hole_count += 1
                     except IndexError:      # top doesn't exist, normal hole
                         hole_count += 1
-        self.debug and print("score:", edge_score)
-        self.debug and print("empty row count:", hole_count)
         total_score, hole_score, bumpiness_score, blockaded_score = (
             0, 0, 0, 0)
         cleared_score = self.calculate_cleared_score(cleared)
@@ -174,9 +157,6 @@ class Agent(object):
             hole_score = self.calculate_hole_score(hole_count)
             bumpiness_score = self.calculate_bumpiness_score(obs)
             blockaded_score = self.calculate_blockaded_score(blockaded_count)
-        self.debug and print("empty cell score:", hole_score)
-        self.debug and print("bumpiness score:", bumpiness_score)
-        self.debug and print("blockaded score:", blockaded_score)
         print("cleared score:", cleared_score)
         total_score = edge_score + hole_score + \
             bumpiness_score + blockaded_score + cleared_score
