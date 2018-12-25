6f534ef770f5089e43bab52f41206cc1867661ea
sami-automatic <sami.mentes@ozu.edu.tr>
2018-12-25 21:49:48 +0300

    DQN_agent fixed

--- DQN_agent.py
+++ DQN_agent.py
@@ -1,76 +1,343 @@
 #My DQN implementation for tetris using keras
+# -*- coding: utf-8 -*-
+import math
+import random
+import sys
+import os
+import shutil
+import numpy as np
+#import matplotlib
+#import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from copy import deepcopy
 
-from __future__ import division
-import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+
 from engine import TetrisEngine
 
-from PIL import Image
-import numpy as np
-import gym
+width, height = 10, 20 # standard tetris friends rules
+engine = TetrisEngine(width, height)
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
-from keras.optimizers import Adam
-import keras.backend as K
+# set up matplotlib
+#is_ipython = 'inline' in matplotlib.get_backend()
+#if is_ipython:
+    #from IPython import display
 
-from rl.agents.dqn import DQNAgent
-from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
-from rl.memory import SequentialMemory
-from rl.core import Processor
-from rl.callbacks import FileLogger, ModelIntervalCheckpoint
+#plt.ion()
 
-class TetrisProcessor(Processor):
-    def process_observation(self, observation):
-        #flatten the board to feed it in to DQN
-        processed_observation = observation.flatten()
-        return processed_observation
+# if gpu is to be used
+use_cuda = torch.cuda.is_available()
+if use_cuda:print("....Using Gpu...")
+FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
+ByteTensor = torch.cuda.ByteTensor if use_cuda else torch.ByteTensor
+#Tensor = FloatTensor
 
-    def process_state_batch(self, batch):
-        # We could perform this processing step in `process_observation`. In this case, however,
-        # we would need to store a `float32` array instead, which is 4x more memory intensive than
-        # an `uint8` array. This matters if we store 1M observations.
-        processed_batch = batch.astype(int)
-        return processed_batch
 
-    def process_reward(self, reward):
-        #DEniz we can implement the reward here coming from environment actually without changing the engine class
+######################################################################
+# Replay Memory
+# -------------
+# -  ``Transition`` - a named tuple representing a single transition in
+#    our environment
+# -  ``ReplayMemory`` - a cyclic buffer of bounded size that holds the
+#    transitions observed recently. It also implements a ``.sample()``
+#    method for selecting a random batch of transitions for training.
+#
 
-        return reward
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 
-# Get the environment and extract the number of actions.
-env = TetrisEngine(10,20)
-nb_actions = 20
-WINDOW_LENGTH = 4
 
-model = Sequential()
-model.add(Dense(32, input_dim = self.state_size, activation = 'relu'))
-model.add(Dense(32), activation = 'relu')
-model.add(Dense(nb_actions, activation = 'linear'))
-model.compile(loss='mse',print(model.summary())
+class ReplayMemory(object):
 
-# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
-# even the metrics!
-memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
-processor = TetrisProcessor()
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
 
-# Select a policy. We use eps-greedy action selection, which means that a random action is selected
-# with probability eps. We anneal eps from 1.0 to 0.1 over the course of 1M steps. This is done so that
-# the agent initially explores the environment (high eps) and then gradually sticks to what it knows
-# (low eps). We also set a dedicated eps value that is used during testing. Note that we set it to 0.05
-# so that the agent still performs some random actions. This ensures that the agent cannot get stuck.
-policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
-                              nb_steps=1000000)
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
 
-# The trade-off between exploration and exploitation is difficult and an on-going research topic.
-# If you want, you can experiment with the parameters or use a different policy. Another popular one
-# is Boltzmann-style exploration:
-# policy = BoltzmannQPolicy(tau=1.)
-# Feel free to give it a try!
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
 
-dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
-               processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
-               train_interval=4, delta_clip=1.)
+    def __len__(self):
+        return len(self.memory)
 
-dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
-dqn.fit(env, callbacks=callbacks, nb_steps=1750000, log_interval=10000)
+class DQN(nn.Module):
+
+    def __init__(self):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        #self.conv3 = nn.Conv2d(32, 32, kernel_size=2, stride=2)
+        #self.bn3 = nn.BatchNorm2d(32)
+        #self.rnn = nn.LSTM(448, 240)
+        self.lin1 = nn.Linear(768, 256)
+        self.head = nn.Linear(256, engine.nb_actions)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        #x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.lin1(x.view(x.size(0), -1)))
+        return self.head(x.view(x.size(0), -1))
+
+
+######################################################################
+# Training
+# --------
+#
+# Hyperparameters and utilities
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# This cell instantiates our model and its optimizer, and defines some
+# utilities:
+#
+# -  ``Variable`` - this is a simple wrapper around
+#    ``torch.autograd.Variable`` that will automatically send the data to
+#    the GPU every time we construct a Variable.
+# -  ``select_action`` - will select an action accordingly to an epsilon
+#    greedy policy. Simply put, we'll sometimes use our model for choosing
+#    the action, and sometimes we'll just sample one uniformly. The
+#    probability of choosing a random action will start at ``EPS_START``
+#    and will decay exponentially towards ``EPS_END``. ``EPS_DECAY``
+#    controls the rate of the decay.
+#
+
+BATCH_SIZE = 64
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+CHECKPOINT_FILE = 'checkpoint.pth.tar'
+
+
+steps_done = 0
+
+model = DQN()
+print(model)
+
+if use_cuda:
+    model.cuda()
+
+loss = nn.MSELoss()
+optimizer = optim.RMSprop(model.parameters(), lr=.001)
+memory = ReplayMemory(3000)
+
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        return model(
+            Variable(state, volatile=True).type(FloatTensor)).data.max(1)[1].view(1, 1)
+    else:
+        return FloatTensor([[random.randrange(engine.nb_actions)]])
+
+
+episode_durations = []
+
+
+'''
+def plot_durations():
+    plt.figure(2)
+    plt.clf()
+    durations_t = torch.FloatTensor(episode_durations)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # Take 100 episode averages and plot them too
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    if is_ipython:
+        display.clear_output(wait=True)
+        display.display(plt.gcf())
+'''
+
+
+######################################################################
+# Training loop
+# ^^^^^^^^^^^^^
+#
+# Finally, the code for training our model.
+#
+# Here, you can find an ``optimize_model`` function that performs a
+# single step of the optimization. It first samples a batch, concatenates
+# all the tensors into a single one, computes :math:`Q(s_t, a_t)` and
+# :math:`V(s_{t+1}) = \max_a Q(s_{t+1}, a)`, and combines them into our
+# loss. By defition we set :math:`V(s) = 0` if :math:`s` is a terminal
+# state.
+
+
+last_sync = 0
+
+
+def optimize_model():
+    global last_sync
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    # Transpose the batch (see http://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation).
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    non_final_mask = ByteTensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)))
+
+    # We don't want to backprop through the expected action values and volatile
+    # will save us on temporarily changing the model parameters'
+    # requires_grad to False!
+    non_final_next_states = Variable(torch.cat([s for s in batch.next_state
+                                                if s is not None]),
+                                     volatile=True)
+    state_batch = Variable(torch.cat(batch.state))
+    action_batch = Variable(torch.cat(batch.action))
+    reward_batch = Variable(torch.cat(batch.reward))
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken
+    state_action_values = model(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    next_state_values = Variable(torch.zeros(BATCH_SIZE).type(FloatTensor))
+    next_state_values[non_final_mask] = model(non_final_next_states).max(1)[0]
+    # Now, we don't want to mess up the loss with a volatile flag, so let's
+    # clear it. After this, we'll just end up with a Variable that has
+    # requires_grad=False
+    next_state_values.volatile = False
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    for param in model.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+    
+    if len(loss.data)>0 : return loss.data[0] 
+    else : return loss
+
+def optimize_supervised(pred, targ):
+    optimizer.zero_grad()
+
+    diff = loss(pred, targ)
+    diff.backward()
+    optimizer.step()
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+def load_checkpoint(filename):
+    checkpoint = torch.load(filename)
+    model.load_state_dict(checkpoint['state_dict'])
+    try: # If these fail, its loading a supervised model
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        memory = checkpoint['memory']
+    except Exception as e:
+        pass
+    # Low chance of random action
+    #steps_done = 10 * EPS_DECAY
+
+    return checkpoint['epoch'], checkpoint['best_score']
+
+if __name__ == '__main__':
+    # Check if user specified to resume from a checkpoint
+    start_epoch = 0
+    best_score = 0
+    if len(sys.argv) > 1 and sys.argv[1] == 'resume':
+        if len(sys.argv) > 2:
+            CHECKPOINT_FILE = sys.argv[2]
+        if os.path.isfile(CHECKPOINT_FILE):
+            print("=> loading checkpoint '{}'".format(CHECKPOINT_FILE))
+            start_epoch, best_score = load_checkpoint(CHECKPOINT_FILE)
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(CHECKPOINT_FILE, start_epoch))
+        else:
+            print("=> no checkpoint found at '{}'".format(CHECKPOINT_FILE))
+
+    ######################################################################
+    #
+    # Below, you can find the main training loop. At the beginning we reset
+    # the environment and initialize the ``state`` variable. Then, we sample
+    # an action, execute it, observe the next screen and the reward (always
+    # 1), and optimize our model once. When the episode ends (our model
+    # fails), we restart the loop.
+
+    f = open('log.out', 'w+')
+    for i_episode in count(start_epoch):
+        # Initialize the environment and state
+        state = FloatTensor(engine.clear()[None,None,:,:])
+
+        score = 0
+        for t in count():
+            # Select and perform an action
+            action = select_action(state).type(LongTensor)
+
+            # Observations
+            last_state = state
+            state, reward, done = engine.step(action[0,0])
+            state = FloatTensor(state[None,None,:,:])
+            
+            # Accumulate reward
+            score += int(reward)
+
+            reward = FloatTensor([float(reward)])
+            # Store the transition in memory
+            memory.push(last_state, action, state, reward)
+
+            # Perform one step of the optimization (on the target network)
+            if done:
+                # Train model
+                if i_episode % 10 == 0:
+                    log = 'epoch {0} score {1}'.format(i_episode, score)
+                    print(log)
+                    f.write(log + '\n')
+                    loss = optimize_model()
+                    if loss:
+                        print('loss: {:.0f}'.format(loss))
+                # Checkpoint
+                if i_episode % 100 == 0:
+                    is_best = True if score > best_score else False
+                    save_checkpoint({
+                        'epoch' : i_episode,
+                        'state_dict' : model.state_dict(),
+                        'best_score' : best_score,
+                        'optimizer' : optimizer.state_dict(),
+                        'memory' : memory
+                        }, is_best)
+                break
+
+    f.close()
+    print('Complete')
+    #env.render(close=True)
+    #env.close()
+    #plt.ioff()
+    #plt.show()
+
